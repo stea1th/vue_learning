1ca5f78cd0a81619403e5f4f887f317a71fbba61
stea1th <stea1th@mail.ru>
2019-08-09 11:34:41 +0200

    new project

---
+++ src/main/java/de/stea1th/vue_learning/VueLearningApplication.java
@@ -0,0 +1,13 @@
+package de.stea1th.vue_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueLearningApplication.class, args);
+    }
+
+}
