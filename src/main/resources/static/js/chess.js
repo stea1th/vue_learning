00d26851254bfca26ca2ac08c33e09b8019aa8fc
stea1th <stea1th@mail.ru>
2019-08-26 12:26:58 +0200

    chess board colored and with ids

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -6,18 +6,24 @@ var chessTitle = new Vue({
 });
 
 Vue.component('chess-cell', {
-    props: ['cell'],
-    template: '<th></th>'
+    props: ['cell', 'num', 'inx'],
+    template: '<th class="cell" :class="testObject"><div :id="cell" :num="num"></div></th>',
+    computed: {
+        testObject: function () {
+            return this.inx % 2 === 0 ? this.num % 2 === 0 ? 'white' : 'black' : this.num % 2 === 0 ? 'black' : 'white'
+        }
+
+    }
 
 });
 
 var chessCell = new Vue({
     el: '#chess-table',
     data: {
+        letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
         cellStyle: {
             height: '50px',
-            width: '50px',
-            border: '1px solid black'
+            width: '50px'
         }
     }
 });
